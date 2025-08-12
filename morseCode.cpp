661158be4e48b44fbd6085496f3708179aa2ff2d
Manganye DL <dumiz4ne@gmail.com>
2025-08-12 10:31:53 +0200

    Update morseCode.cpp
    Validate user input and print the letter-morse code pairs accordingly.

--- morseCode.cpp
+++ morseCode.cpp
@@ -20,3 +20,21 @@ int main() {
     getline(cin, input);
     // Output variable to display the full morse code message
     string output{};
+    // Check if user enters no input
+    if (input.empty()) {
+        cout << "No input provided." << endl;
+    // Immediately exit the program
+        return 1;
+    } else {
+        // Convert each letter into uppercase
+        for (char c : input) {
+            c = toupper(c);  // Convert letters into uppercase
+            // If the charecters in the user input exist in the map container, print their morse code equivalents
+            if (morseCode.find(c) != morseCode.end()) {
+                cout << c << ": " << morseCode[c] << endl;
+                output += morseCode[c] + " ";
+            } else if (c == ' ') {
+                output += "   ";  // Add three spaces to the 'output' variable
+                cout << endl;  // Skip a line
+            }
+        }

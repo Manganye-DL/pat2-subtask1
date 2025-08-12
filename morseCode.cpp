109d6f863b3ff819115c5fcb76d456d9ae9fce0f
Manganye DL <dumiz4ne@gmail.com>
2025-08-12 10:21:40 +0200

    Create morseCode.cpp
    Created a map container to store my letter-morse code pairs.

---
+++ morseCode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    // A container (map) for key-value pairs of alphabets and their morse code equivalents
+    map<char, string> morseCode = {
+        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
+        {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
+        {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
+        {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."},
+        {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+        {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
+        {'Y', "-.--"}, {'Z', "--.."}
+    };
